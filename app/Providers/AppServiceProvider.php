88ffc99f97a59d2d5e6f807d05071765b3faef18
xiaomlove <i353856593@163.com>
2022-11-17 17:04:13 +0800

    install filament

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Filament\Resources\UserResource;
+use Filament\Facades\Filament;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Filament::registerResources([UserResource::class]);
     }
 
     /**
